1e38c650d766d7a8ded925423319b56b40f0a1cb
TheSoftNode <thesoftnode@gmail.com>
2023-06-16 07:04:12 -0700

    Corrected the betty style errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 
-int main(void){
-	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	system( "echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'" );
 	return(1);
 }
