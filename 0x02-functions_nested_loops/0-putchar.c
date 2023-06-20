69180df46c8f9ab215f345045718da97fdeb672d
TheSoftNode <thesoftnode@gmail.com>
2023-06-20 08:44:14 -0700

    Added a program that prints _putchar, followed by a new line. And updated the newly created README.md file accordingly

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar to stdout
+ *
+ * Return: Always zero (0)
+ */
+int main(void)
+{
+	puts("_putchar");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Functions and nested loop functions
+
+1. 0-putchar.c: A program that prints _putchar, followed by a new line.
