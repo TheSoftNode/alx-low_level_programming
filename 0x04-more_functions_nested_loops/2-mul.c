67d11c8ffe64b6cf5d4141e08762e96058dafd96
TheSoftNode <thesoftnode@gmail.com>
2023-06-22 16:38:41 -0700

    Added a function that multiplies two integers. And updated the README.md file accordingly

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Multiply 2 integers.
+ * @a: An integer to be multiplied with b
+ * @b : An integer to be multipled with a
+ * Return: a multiplied by b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,5 @@ More functions and Nested Loops
 1. 0-isupper.c: A function that checks for uppercase character.
 
 2. 1-isdigit.c: A function that checks for a digit (0 through 9).
+
+3. 2-mul: A function that multiplies two integers.
